2988a2d217f0b97dae64021b0d84c4a0fbf5a38f
reisenhead <85323168+reisenhead@users.noreply.github.com>
2021-07-10 20:21:06 -0500

    Agregando retos y postwork

---
+++ src/retosFinalyPostwork/s1/postWork.kt
@@ -0,0 +1,29 @@
+package retosFinalyPostwork.s1
+
+// Tenemos varias implementaciones para este PostWork les mostraremos algunas de ellas implementadas
+// en nuestro proyecto
+
+//PostWork 1
+
+/* operaciones de recibir datos y guardar en una variable, realizando operaciones
+fragmento tomado del package controllers archivo carritoControllers
+
+ println("*************************************************")
+    println("*  Ingrese la cantidad de articulos             *")
+    println("*************************************************")
+    var opcion3 = readLine()!!.toInt() <-----------
+    var opc3 = arrayListOf<Int>()
+    opc3.add(opcion3)
+
+    var Tot = opcion3*precio1   <----------
+    Totalprecio = Totalprecio+Tot
+
+    var espacios ="  "
+    var espaciosl ="      "
+    var signo = "$"
+    var disponibles = stok - opcion3  <----------
+
+*/
+
+
+
