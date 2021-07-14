6b8a2d379234b6988e86775e1d7b3be5b4f72306
Crystian Octavio Hernandez Lopez <85323168+reisenhead@users.noreply.github.com>
2021-07-13 23:24:12 -0500

    Reto final y postwork 7

--- src/retosFinalyPostwork/s7/postWork7.kt
+++ src/retosFinalyPostwork/s7/postWork7.kt
@@ -1,2 +1,65 @@
 package retosFinalyPostwork.s7
 
+// Postwork 7
+
+//Obtenido de nuestro proyecto
+
+/*
+fun menu(){
+
+    val opcion: Int?
+    try {
+
+
+        println("*****MENU******")
+        println("* Por favor selecciona una opcion:*")
+        println("*  1- Iniciar sesion:             *")
+        println("*  2- Entrar como invitado:       *")
+        println("*  3- Crea tu cuenta:             *")
+        println("*  4- Salir:                      *")
+        println("*************")
+
+        println("\nIngresa un digito:")
+        opcion = readLine()?.toInt()
+
+    } catch(e: NumberFormatException) {  // Nos dice que solo debemos poner numeros.
+        println("********************")
+        println("La opcion solo acepta valor numerico,es decir del 1 al 4 ")
+        println("********************")
+        return menu()
+    }
+    when (opcion) {
+        1 -> iniciarSesion()
+        2 -> menuInicioInvitado()
+        3 -> registrarNuevoUsuarioCliente()//funcion para crear un nuevo usuario y cliente
+        4 -> print("GRACIAS; REGRESA PRONTO")
+    }
+}*/
+
+
+/*
+fun main() {
+    // .1
+    fun divide(num1:Int,num2:Int): Int{
+        return try { num1/num2} catch (e : Exception){0}
+    }
+
+    println(divide(10,0))
+    println(divide(10,2))
+
+    // .2
+    val me = Person(name = "Andres", visitedCities = listOf("Mexico", null, "Brasil"))
+
+//    me.visitedCities?.forEach {
+//        println("Cities -> ${it ?: throw IllegalArgumentException("Ciudad vacía")}")
+//    }
+
+    fun fail(message: String): Nothing {
+        throw IllegalArgumentException(message)
+    }
+
+    val you = Person()
+    val name = you.name ?: fail("No existe nombre")
+    println(name)
+
+ */
--- src/retosFinalyPostwork/s7/retoFinal7.kt
+++ src/retosFinalyPostwork/s7/retoFinal7.kt
@@ -1,2 +1,29 @@
 package retosFinalyPostwork.s7
 
+// Reto final 7
+
+// en nuestra aplicacion desde todas las lecturas de utilizan try catch para validar opciones correctas
+// de lo que se lee
+
+// Fragmento tomado del main del package views
+
+//try {
+//
+//
+//    println("****************MENU***************")
+//    println("* Por favor selecciona una opcion:*")
+//    println("*  1- Iniciar sesion:             *")
+//    println("*  2- Entrar como invitado:       *")
+//    println("*  3- Crea tu cuenta:             *")
+//    println("*  4- Salir:                      *")
+//    println("***********************************")
+//
+//    println("\nIngresa un digito:")
+//    opcion = readLine()?.toInt()
+//
+//} catch(e: NumberFormatException) {
+//    println("************************************************************")
+//    println("La opcion solo acepta valor numerico,es decir del 1 al 4 ")
+//    println("************************************************************")
+//    return menu()
+//}
